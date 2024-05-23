4ac3772dddb21e7057297ea9a5478113caef3be0
rehema <gatierehema@gmail.com>
2024-05-23 16:30:54 +0300

    create cos-accounting

--- src/app/cost-accounting/cost-accounting.component.ts
+++ src/app/cost-accounting/cost-accounting.component.ts
@@ -1,22 +1,67 @@
-import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'angular-highcharts';
+
 
 @Component({
   selector: 'app-cost-accounting',
   templateUrl: './cost-accounting.component.html',
   styleUrls: ['./cost-accounting.component.css']
 })
-export class CostAccountingComponent {
-  assetValue: number = 0;
-  usefulLife: number = 0;
-  salvageValue: number = 0;
+export class CostAccountingComponent implements OnInit {
+  assetValue: number = 50000;
+  usefulLife: number = 10;
+  salvageValue: number = 1000;
   annualDepreciation: number = 0;
-depreciation: any;
+  depreciation: number | null = null;
+  chart: Chart | undefined;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
   calculateDepreciation(): void {
     if (this.usefulLife > 0) {
       this.annualDepreciation = (this.assetValue - this.salvageValue) / this.usefulLife;
+      this.depreciation = this.annualDepreciation;
     } else {
       this.annualDepreciation = 0;
+      this.depreciation = null;
     }
+
+    // Update the chart
+    this.updateChart();
+  }
+
+  updateChart(): void {
+    const years = Array.from({ length: this.usefulLife }, (_, i) => `Year ${i + 1}`);
+    const depreciationValues = Array.from({ length: this.usefulLife }, () => this.annualDepreciation);
+
+    this.chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Depreciation Over Time'
+      },
+      credits: {
+        enabled: false
+      },
+      xAxis: {
+        categories: years
+      },
+      yAxis: {
+        title: {
+          text: 'Value'
+        }
+      },
+      series: [
+        {
+          name: 'Depreciation',
+          type: 'line',
+          data: depreciationValues
+        }
+      ]
+    });
   }
 }
